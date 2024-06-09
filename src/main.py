af4ee67ead53bf9230590f8ff9d8e04625146e0e
Gabe Odachowski <gabe927@gmail.com>
2024-06-09 14:38:28 -0400

    moved some stuff into functions

--- src/main.py
+++ src/main.py
@@ -8,52 +8,53 @@ logging.basicConfig(level=logging.DEBUG)
 # Define the Unifi controller IP address
 unifi_controller_ip = "192.168.100.1"
 
+# Define your credentials
+username = "api2"
+password = "HarfordSound69"
+
 # Define the URL for logging in
 login_url = f"https://{unifi_controller_ip}/api/auth/login"
 
 # Define the URL for getting device status
 device_status_url = f"https://{unifi_controller_ip}/proxy/network/api/s/default/stat/device/"
 
-# Define your credentials
-username = "api2"
-password = "HarfordSound69"
-
 # Create a session object
 session = requests.Session()
-
-# Login to the Unifi controller
-login_data = {
-    "username": username,
-    "password": password
-}
 
 # Disable SSL certificate verification
 session.verify = False
 
-response = session.post(login_url, json=login_data)
+def login():
+    # Login to the Unifi controller
+    login_data = {
+        "username": username,
+        "password": password
+    }
 
-# Check if login was successful
-if response.status_code != 200:
-    print("Login failed!")
-    print(response.json())
-    exit()
+    response = session.post(login_url, json=login_data)
+    # Check if login was successful
+    if response.status_code != 200:
+        print("Login failed!")
+        print(response.json())
+        return False
+    else:
+        print("Login successful!")
+        return True
+    
+def getSnakeStatus():
+    # Get device status
+    device_status_response = session.get(device_status_url)
 
-print("Login successful!")
+    # Check if getting device status was successful
+    if device_status_response.status_code != 200:
+        print("Failed to get device status!")
+        return False, {"Error":f"Unifi API returned {device_status_response.status_code} status code"}
 
-# Get device status
-device_status_response = session.get(device_status_url)
+    # get snake status
+    snake_status_class = snake_status.snake_status()
+    device_status = device_status_response.json()["data"]
+    snake_status_result = snake_status_class.run(device_status)
+    return True, snake_status_result
 
-# Check if getting device status was successful
-if device_status_response.status_code != 200:
-    print("Failed to get device status!")
-    exit()
-
-# Print device status
-# print("Device Status:")
-# print(type(device_status_response.json()))
-
-# get snake status
-snake_status_class = snake_status.snake_status()
-device_status = device_status_response.json()["data"]
-snake_status_result = snake_status_class.run(device_status)
-print(snake_status_result)
+if __name__ == "__main__":
+    login()
